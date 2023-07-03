4f91d4f7b3a1440fefa0ddf1851858f70815e6a0
KARAN PANCHAL <83541879+KaranP666@users.noreply.github.com>
2023-07-03 23:25:24 +0530

    Handlesubmit and Handlechange function are updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,8 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
+
+
 const Contact = () => {
   const formRef = useRef();
   const [form , setForm] = useState({
@@ -14,8 +16,38 @@ const Contact = () => {
     message: ''
   });
   const [loading , setLoading] = useState(false);
-  const handleChange = (e) =>{}
-  const handleSubmit = (e) =>{}
+  const handleChange = (e) =>{
+    const { target } = e;
+    const { name, value } = target;
+    setForm({ ...form , [name]:value, });
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send('service_9ifvvzs','template_u5qansb',
+                  {
+                    from_name: form.name,
+                    to_name: 'Karan',
+                    from_email: form.email,
+                    to_email: 'karan.h.panchal786@gmail.com',
+                    message: form.message,
+                  },
+                  'SxuRSvHGlSSU2ohdB')
+                  .then(()=>{
+                    setLoading(false);
+                    alert('Thank you. I will get back to you as soon as possible.');
+                    setForm({
+                      name:'',
+                      email:'',
+                      message:'',
+                    })
+                  }, (error)=>{
+                    setLoading(false)
+                    console.log(error);
+                    alert('Somthing went wrong.')
+                  })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
